91f5a16afe58e4a46e074ac81736e207626674b2
iambrij <bhattbrijraj568@gmail.com>
2020-08-27 03:18:54 -0700

    removed unnecessary things from the home page

--- resources/views/front/home.blade.php
+++ resources/views/front/home.blade.php
@@ -36,41 +36,6 @@
             <a href="{{route('products')}}"><button class="button large">Check out My Shirts</button></a>
         </section>
         <br/>
-        <div class="subheader text-center">
-             <h2>
-            Mykart Latest Products:
-        </h2>
-
-         <!-- Latest SHirts -->
-         <div class="row">
-            <div class="small-3 columns">
-                @foreach ($products as $product)
-                    
-               
-                <div class="item-wrapper">
-                    <div class="img-wrapper">
-                        
-                        <a href="#">
-                           
-                            <img src="{{asset($product->image)}}"/>
-                        </a>
-                    </div>
-                <a href="{{route('product', ['id' => $product->id])}}">
-                        <h3>
-                            {{$product->name}}
-                        </h3>
-                    </a>
-                    <h5>
-                        ₹{{$product->price}}
-                    </h5>
-                    
-               
-                  
-                </div>
-                @endforeach
-                
-            </div>
-        </div>
        
        
             
--- resources/views/front/success.blade.php
+++ resources/views/front/success.blade.php
@@ -1,10 +1,30 @@
 @extends('layout.main')
 @section('content')
 
-<h3>Your order has been placed. Thanks for shopping with us</h3>
+<h3>Your order has been placed. Thanks for shopping with us. </h3>
+<h3>We are forwarding you a confirmation message shortly.</h3>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
 
-
-
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
 
 
 @endsection
